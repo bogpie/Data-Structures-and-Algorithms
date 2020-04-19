6d2d1fa8ac2c2bd711fbc57b38923fd00a815262
Bogdan Piele <bogdanpielemail@gmail.com>
2020-04-19 20:10:08 +0300

    add

--- lab/lab_09_graf/piele_bogdan_lab_09.c
+++ lab/lab_09_graf/piele_bogdan_lab_09.c
@@ -10,11 +10,11 @@ struct GraphMat
 	int** mat;
 };
 
-typedef struct Node Node;
-struct Node
+typedef struct ListNode ListNode;
+struct ListNode
 {
 	int index;
-	struct Node* next;
+	struct ListNode* next;
 };
 
 typedef struct GraphList GraphList;
@@ -22,7 +22,7 @@ struct GraphList
 {
 	int nrVertexes;
 	int nrEdges;
-	Node** vLists;
+	ListNode** vLists;
 };
 
 void fPointerTest(void* ptr)
@@ -33,27 +33,17 @@ void fPointerTest(void* ptr)
 	}
 }
 
-void fCreateGraphMat(GraphMat** adrGraphMat)
+void fCreateGraphMat(GraphMat* graphMat)
 {
-	GraphMat* graphMat = malloc(sizeof(GraphMat));
-	fPointerTest(graphMat);
 	printf("\nnr noduri:");
-	scanf("%d", &graphMat->nrVertexes);
 	int nrVertexes = graphMat->nrVertexes;
-	graphMat->mat = calloc(nrVertexes , sizeof(int*));
-
-	for (int i = 0; i < nrVertexes; ++i)
-	{
-		graphMat->mat[i] = calloc(nrVertexes , sizeof(int));
-	}
-
-	fPointerTest(graphMat->mat);
+	
 	graphMat->nrEdges = 0;
 	for (int i = 0; i < nrVertexes; ++i)
 	{
 		for (int j = i + 1; j < nrVertexes; ++j)
 		{
-			printf("\n arc de la %d la %d ( valoare boolean ) : ",i,j);
+			printf("\n arc de la %d la %d ( valoare boolean ) : ", i, j);
 			scanf("%d", &graphMat->mat[i][j]);
 			if (graphMat->mat[i][j])
 			{
@@ -62,44 +52,44 @@ void fCreateGraphMat(GraphMat** adrGraphMat)
 			graphMat->mat[j][i] = graphMat->mat[i][j];
 		}
 	}
-	*adrGraphMat = graphMat;
 }
 
 void fInsertEdge(GraphList* graphList, int i, int j)
 {
-	Node* newNode = malloc(sizeof(newNode));
-	newNode->next = NULL;
-	newNode->index = j;
-	Node* oldNext = graphList->vLists[i]->next;
-	graphList->vLists[i]->next = newNode;
-	newNode->next = oldNext;
-	
+	ListNode* newListNode = malloc(sizeof(newListNode));
+	newListNode->next = NULL;
+	newListNode->index = j;
+	ListNode* oldNext = graphList->vLists[i]->next;
+	graphList->vLists[i]->next = newListNode;
+	newListNode->next = oldNext;
+
 }
 
 
-void fInitGraphList(GraphList** adrGraphList,int nrVertexes)
+void fInitGraphList(GraphList** adrGraphList, int nrVertexes)
 {
 	GraphList* graphList = *adrGraphList;
 	graphList = malloc(sizeof(GraphList));
 	graphList->nrEdges = 0;
-	graphList->nrVertexes= 0;
-	graphList->vLists = malloc(sizeof(Node*)*nrVertexes);
+	graphList->nrVertexes = 0;
+	graphList->vLists = malloc(sizeof(ListNode*) * nrVertexes);
 	*adrGraphList = graphList;
 
 	for (int i = 0; i < nrVertexes; ++i)
 	{
-		graphList->vLists[i] = malloc(sizeof(Node*));
+		graphList->vLists[i] = malloc(sizeof(ListNode*));
 		graphList->vLists[i]->index = i;
 		graphList->vLists[i]->next = NULL;
 	}
 	*adrGraphList = graphList;
 }
 
-void fTransform(GraphMat* graphMat, GraphList** adrGraphList)
+void fMatToList(GraphMat* graphMat, GraphList** adrGraphList)
 {
-	GraphList* graphList=*adrGraphList;
+	GraphList* graphList = *adrGraphList;
 
-	int nrVertexes = graphMat->nrVertexes;
+	int nrVertexes = graphList->nrVertexes=graphMat->nrVertexes;
+	graphList->nrEdges = graphMat->nrEdges;
 	for (int i = 0; i < nrVertexes; ++i)
 	{
 		for (int j = 0; j < nrVertexes; ++j)
@@ -113,15 +103,110 @@ void fTransform(GraphMat* graphMat, GraphList** adrGraphList)
 	*adrGraphList = graphList;
 }
 
+void fListToMat(GraphList* graphList, GraphMat** adrGraphMat)
+{
+	GraphMat* graphMat = *adrGraphMat;
+	int nrVertexes = graphMat->nrVertexes = graphList->nrVertexes;
+	graphMat->nrEdges = graphMat->nrEdges;
+	for (int i = 0; i < nrVertexes; ++i)
+	{
+		int index1 = graphList->vLists[i]->index;
+		ListNode* listNode = graphList->vLists[i]->next;
+		while (listNode)
+		{
+			int index2 = listNode->index;
+			graphMat->mat[index1][index2] = 1;
+			listNode = listNode->next;
+		}
+	}
+}
+
+void fEraseGraphMat(GraphMat* graphMat)
+{
+	for (int i = 0; i < graphMat->nrVertexes; ++i)
+	{
+		int j;
+		for (j = 0; j < graphMat->nrVertexes; ++j);
+		{
+			graphMat->mat[i][j] = 0;
+		}
+		/*
+		if (graphMat->mat[i] != NULL)
+		{
+			free(graphMat->mat[i]);
+		}
+		*/
+	}
+	free(graphMat);
+	graphMat->nrEdges = 0;
+	graphMat->nrVertexes = 0;
+}
+
+void fPrintGraphList(GraphList* graphList)
+{
+	for (int i = 0; i < graphList->nrVertexes; ++i)
+	{
+		ListNode* listNode = graphList->vLists[i];
+		printf("\n%d: ",graphList->vLists[i]->index);
+		listNode = listNode->next;
+		while (listNode!=NULL)
+		{
+			printf("%d ", listNode->index);
+			listNode = listNode->next;
+		}
+		printf("\n");
+	}
+}
+
+void fPrintGraphMat(GraphMat* graphMat)
+{
+	int nrVertexes = graphMat->nrVertexes;
+	for (int i = 0; i < nrVertexes; ++i)
+	{
+		for (int j = 0; j < nrVertexes; ++j)
+		{
+			printf("%d ",graphMat->mat[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void fInitGraphMat(GraphMat** adrGraphMat,int nrVertexes)
+{
+	GraphMat* graphMat = malloc(sizeof(GraphMat));
+	fPointerTest(graphMat);
+	*adrGraphMat = graphMat;
+	graphMat->mat = calloc(nrVertexes, sizeof(int*));
+
+	for (int i = 0; i < nrVertexes; ++i)
+	{
+		graphMat->mat[i] = calloc(nrVertexes, sizeof(int));
+	}
+
+	fPointerTest(graphMat->mat);
+}
 
 int main()
 {
-	printf("%d", atoi("-1123"));
 	printf("se vor crea grafuri neorientate cu noduri de la 0 la nr de noduri-1");
-	GraphMat* graphMat =NULL;
-	GraphList* graphList=NULL;	// le initializez in functiile de jos
-	fCreateGraphMat(&graphMat);
-	fInitGraphList(&graphList,graphMat->nrVertexes);
-	fTransform(graphMat, &graphList);
+	GraphMat* graphMat = NULL;
+	GraphList* graphList = NULL;	// le initializez in functiile de jos
+	int nrVertexes;
+	printf("\nnr de noduri:");
+	scanf("%d", &nrVertexes);
+	fInitGraphMat(&graphMat, nrVertexes);
+
+	fCreateGraphMat(graphMat);
+	fInitGraphList(&graphList, graphMat->nrVertexes);
+	fMatToList(graphMat, &graphList);
+	printf("\nam creat graful cu liste din graful cu matrice (de adiacenta):\n");
+	fPrintGraphList(graphList);
+
+	fEraseGraphMat(graphMat);
+	fInitGraphMat(&graphMat,nrVertexes);
+	fListToMat(graphList, &graphMat);
+	printf("\nam creat graful cu matrice din graful cu liste:\n");
+	fPrintGraphMat(graphMat);
+
 	return 0;
 }
